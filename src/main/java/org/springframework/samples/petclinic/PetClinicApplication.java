83d395a5092b5c8725ad019968d66fed4e0d480e
chenxinzhou <jojolocklock@gmail.com>
2025-01-02 20:44:17 +0800

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -30,6 +30,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
